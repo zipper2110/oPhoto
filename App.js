ebf164cf27fc3280c3dd24c6f71654e927e8d4f2
Dmitry Litvin <lee-tvin@mail.ru>
2018-11-17 18:34:41 +0500

    1st view ready

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+constructor(props) {
+  super(props);
+  this.state = { clickedButton: "none" };
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Feck usdfuu</Text>
+        <Text>What you want?</Text>
+        <Button
+          onPress={() => {this.onPressTranslate()}}
+          title="Translate"
+          color="#841584"
+          accessibilityLabel=""
+        />
+        <Button
+          onPress={() => {this.onPressLook()}}
+          title="Look"
+          color="#841584"
+          accessibilityLabel=""
+        />
+        <Text>Button clicked: {this.state.clickedButton}</Text>
       </View>
     );
+  }
+
+  onPressTranslate() {
+    this.clickButton("Translate");
+  }
+
+  onPressLook() {
+    this.clickButton("Look");
+  }
+
+  clickButton(buttonName) {
+    this.setState({clickedButton: buttonName});
   }
 }
 
