7830d4f87ae154f4aada7ff816f062d7c6e5e82d
Daria Gorskikh <gorskikh@tochka.com>
2018-11-23 17:27:33 +0500

    first screen new styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -11,20 +11,20 @@ constructor(props) {
   render() {
     return (
       <View style={styles.container}>
-        <Text>What you want?</Text>
-        <Button
-          onPress={() => {this.onPressTranslate()}}
-          title="Translate"
-          color="#841584"
-          accessibilityLabel=""
-        />
-        <Button
-          onPress={() => {this.onPressLook()}}
-          title="Look"
-          color="#841584"
-          accessibilityLabel=""
-        />
-        <Text>Button clicked: {this.state.clickedButton}</Text>
+        <Text style={styles.text}>What do you want?</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            buttonStyle={styles.button}
+            onPress={() => {this.onPressTranslate()}}>
+              <Text style={styles.button}>Share</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            buttonStyle={styles.button}
+            onPress={() => {this.onPressLook()}}>
+            <Text style={styles.button}>Look</Text>
+          </TouchableOpacity>
+        </View>
+        {/*<Text>Button clicked: {this.state.clickedButton}</Text>*/}
       </View>
     );
   }
@@ -47,6 +47,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  text: {
+    fontSize: 26,
+    color: 'black',
+    fontWeight: '600',
+    marginBottom: 40
+  },
+  button: {
+    width: 120,
+    color: '#9c8762',
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 10,
+    borderWidth: 4,
+    borderStyle: 'solid',
+    borderColor: '#9c8762'
+  }
 });
